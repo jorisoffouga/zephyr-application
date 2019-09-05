4af79dcb96cc79bdae3c1b978a4bea4a923f0e2a
Joris Offouga <offougajoris@gmail.com>
2019-09-05 17:23:32 +0200

    sample: button_led: Update with new zephyr api
    Signed-off-by: Joris Offouga <offougajoris@gmail.com>

--- samples/button_led/src/main.c
+++ samples/button_led/src/main.c
@@ -4,14 +4,35 @@
 #include <device.h>
 #include <gpio.h>
 
-#define GPIO_DRV_NAME SW0_GPIO_CONTROLLER
-#define LED0 LED0_GPIO_PIN
-#define LED1 LED1_GPIO_PIN
-#define GPIO_KEY_PIN SW0_GPIO_PIN
-#define EDGE (GPIO_INT_EDGE | GPIO_INT_ACTIVE_LOW)
-#define PULL_UP 0
+#define SW0_PORT DT_ALIAS_SW0_GPIOS_CONTROLLER
+#define SW0_PIN DT_ALIAS_SW0_GPIOS_PIN
+
+#define LED0 DT_ALIAS_LED0_GPIOS_PIN
+#define LED_PORT DT_ALIAS_LED0_GPIOS_CONTROLLER
+
+/* change to use another GPIO pin interrupt config */
+#ifdef DT_ALIAS_SW0_GPIOS_FLAGS
+#define EDGE    (DT_ALIAS_SW0_GPIOS_FLAGS | GPIO_INT_EDGE)
+#else
+/*
+ * If DT_ALIAS_SW0_GPIOS_FLAGS not defined used default EDGE value.
+ * Change this to use a different interrupt trigger
+ */
+#define EDGE    (GPIO_INT_EDGE | GPIO_INT_ACTIVE_LOW)
+#endif
+
+/* change this to enable pull-up/pull-down */
+#ifndef DT_ALIAS_SW0_GPIOS_FLAGS
+#ifdef DT_ALIAS_SW0_GPIOS_PIN_PUD
+#define DT_ALIAS_SW0_GPIOS_FLAGS DT_ALIAS_SW0_GPIOS_PIN_PUD
+#else
+#define DT_ALIAS_SW0_GPIOS_FLAGS 0
+#endif
+#endif
+#define PULL_UP DT_ALIAS_SW0_GPIOS_FLAGS
 
 static struct gpio_callback gpio_cb;
+static struct device *led;
 
 static int gpio_pin_toggle(struct device *port, u32_t pin)
 {
@@ -40,64 +61,57 @@ void gpio_callback(struct device *port,
 
     printk("Button pressed at %d\n", k_cycle_get_32());
 
-    ret = gpio_pin_toggle(port, LED0);
+    ret = gpio_pin_toggle(led, LED0);
 
     if (ret)
     {
-        printk("Error set " GPIO_DRV_NAME "%d!\n", LED0);
-    }
-
-    ret = gpio_pin_toggle(port, LED1);
-
-    if (ret)
-    {
-        printk("Error set " GPIO_DRV_NAME "%d!\n", LED1);
+        printk("Error set " LED_PORT "%d!\n", LED0);
     }
 }
 
 void main(void)
 {
-    struct device *gpio_dev;
     int ret;
+    struct device *button_dev;
 
-    gpio_dev = device_get_binding(GPIO_DRV_NAME);
+    button_dev = device_get_binding(SW0_PORT);
 
-    if (!gpio_dev)
+    if (!button_dev)
     {
-        printk("Cannot find %s!\n", GPIO_DRV_NAME);
+        printk("Cannot find %s!\n", SW0_PORT);
         return;
     }
 
-    ret = gpio_pin_configure(gpio_dev, LED0, (GPIO_DIR_OUT));
+	led = device_get_binding(LED_PORT);
+
+	if(!led){
+		printk("Cannot find %s!\n", SW0_PORT);
+        return;
+	}
+
+    ret = gpio_pin_configure(led, LED0, (GPIO_DIR_OUT));
 
     if (ret)
     {
-        printk("Error configuring " GPIO_DRV_NAME "%d!\n", LED0);
+        printk("Error configuring " LED_PORT "%d!\n", LED0);
     }
 
-    ret = gpio_pin_configure(gpio_dev, LED1, (GPIO_DIR_OUT));
-
+    ret = gpio_pin_configure(button_dev, SW0_PIN, GPIO_DIR_IN | GPIO_INT | PULL_UP | EDGE);
     if (ret)
     {
-        printk("Error configuring " GPIO_DRV_NAME "%d!\n", LED1);
+        printk("Error configuring " SW0_PORT "%d!\n", SW0_PIN);
     }
 
-    ret = gpio_pin_configure(gpio_dev, GPIO_KEY_PIN, GPIO_DIR_IN | GPIO_INT | PULL_UP | EDGE);
-    if (ret)
-    {
-        printk("Error configuring " GPIO_DRV_NAME "%d!\n", GPIO_KEY_PIN);
-    }
+    gpio_init_callback(&gpio_cb, gpio_callback, BIT(SW0_PIN));
 
-    gpio_init_callback(&gpio_cb, gpio_callback, BIT(GPIO_KEY_PIN));
-
-    ret = gpio_add_callback(gpio_dev, &gpio_cb);
+    ret = gpio_add_callback(button_dev, &gpio_cb);
 
     if (ret)
     {
         printk("Cannot setup callback!\n");
     }
 
-    ret = gpio_pin_enable_callback(gpio_dev, GPIO_KEY_PIN);
+    ret = gpio_pin_enable_callback(button_dev, SW0_PIN);
 
     if (ret)
     {
