97770f44dad99922f991ddfe2915c681cffd66cb
Joris Offouga <offougajoris@gmail.com>
2020-08-26 10:39:45 +0200

    Add dts_root and board_root in module.yml
    Signed-off-by: Joris Offouga <offougajoris@gmail.com>

--- cmake/boilerplate.cmake
+++ cmake/boilerplate.cmake
@@ -1,6 +1,1 @@
-set(ZA_DIR ${CMAKE_CURRENT_LIST_DIR}/..)
-# Add ZA_DIR as a BOARD_ROOT in case the board is in ZA_DIR
-list(APPEND BOARD_ROOT ${ZA_DIR})
-
-# Add ZA_DIR as a DTS_ROOT to include dts
-list(APPEND DTS_ROOT ${ZA_DIR})
+#set(ZA_DIR ${CMAKE_CURRENT_LIST_DIR}/..)
